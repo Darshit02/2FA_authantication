8c93e34fd632e51b43bae9d8a4d45b3bb63cfec5
Darshit Mistry <darshitmistry2@gmail.com>
2024-07-09 18:38:32 +0530

    feat : user button

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,19 @@
+import Navbar from "./_components/navbar";
+
+interface ProtectedLayoutProps {
+  children: React.ReactNode;
+}
+
+const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
+  return (
+    <div
+      className="h-full w-full flex gap-y-10 flex-col justify-center items-center
+  bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800
+  ">
+    <Navbar/>
+      {children}
+    </div>
+  );
+};
+
+export default ProtectedLayout;
---
+++ components/auth/user-button.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+ } from "@/components/ui/dropdown-menu"
+
+import { LogOutIcon, User2 } from "lucide-react"
+
+import {
+    Avatar,
+    AvatarImage,
+    AvatarFallback
+} from "@/components/ui/avatar"
+import { useCurrentUser } from "@/hooks/use-current-user"
+import { LogoutButton } from "./logout-button"
+
+export const UserButton = () =>{
+    const user = useCurrentUser()
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                    <AvatarImage src={user?.image || ""}/>
+                    <AvatarFallback className="bg-sky-500">
+                        <User2 className="text-white"/>
+                    </AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-40" align="end">
+                <LogoutButton>
+                    <DropdownMenuItem >
+                        <LogOutIcon className="h-4 w-4 mr-2"/>
+                        Logout
+                    </DropdownMenuItem>
+                </LogoutButton>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
