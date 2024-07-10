8764eb06bed3f19f07130892850460306013c005
Darshit Mistry <darshitmistry2@gmail.com>
2024-07-10 10:57:46 +0530

    feat : settings page init

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,15 +1,98 @@
-"use client"
-import logout from "@/actions/logout";
-import { Button } from "@/components/ui/button";
-import { useCurrentUser } from "@/hooks/use-current-user";
+"use client";
 
-const SettingsPage =  () => {
+import { useState, useTransition } from "react";
+import { useSession } from "next-auth/react";
+import { settings } from "@/actions/settings";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { Settings } from "lucide-react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SettingsSchema } from "@/schemas";
+import FormSuccess from "@/components/form-success";
+import FormError from "@/components/form-error";
+
+const SettingsPage = () => {
+  const user = useCurrentUser();
+  const [error, setError] = useState<string>();
+  const [success, setSuccess] = useState<string>();
+  const { update } = useSession();
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof SettingsSchema>>({
+    resolver: zodResolver(SettingsSchema),
+    defaultValues: {
+      name: user?.name || undefined,
+      email: user?.email || undefined,
+      isTwoFactorEnabled: user?.isTwoFactorEnabled || undefined,
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
+    startTransition(() => {
+      settings(values)
+        .then((data) => {
+          if (data.error) {
+            setError("Error Updating Settings");
+          }
+          if (data.success) {
+            update();
+            setSuccess("Settings Updated Successfully");
+          }
+        })
+        .catch(() => setError("Error Updating Settings"));
+    });
+  };
 
   return (
-    <div className="bg-white p-10 rounded-xl">
-   settings
-      </div>
-  )
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-2xl font-semibold flex justify-center items-center flex-row gap-x-2">
+          <Settings />
+          <span>Settings</span>
+        </p>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Name"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormError message={error} />
+            <FormSuccess message={success} />
+            <Button type="submit" disabled={isPending}>
+              Save
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default SettingsPage;
